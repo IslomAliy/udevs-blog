f51607d452690211ff9a3acf48950e7a2ed0204e
islomaliy <kislombek71@gmail.com>
2021-12-08 10:16:45 +0500

    logOut page refresh issue fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,31 +5,37 @@ import { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const isAuthorized = localStorage.getItem('isAuthorized');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isAuthorized = localStorage.getItem("isAuthorized");
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const staticUser = {
-      email: 'falonchi@gmail.com',
-      password: '12345'
-    }
+      email: "falonchi@gmail.com",
+      password: "12345",
+    };
 
-    if(email=== staticUser.email && password === staticUser.password ){
-      localStorage.setItem('isAuthorized', true);
-      setIsOpen(false)
+    if (email === staticUser.email && password === staticUser.password) {
+      localStorage.setItem("isAuthorized", true);
+      setIsOpen(false);
+    } else {
+      alert(
+        "If you don't know exact login and password, you'd better get lost!"
+      );
     }
-    else {
-      alert("If you don't know exact login and password, you'd better get lost!")
-    }
+  };
+
+  const logOut = () => {
+    localStorage.removeItem('isAuthorized')
+    document.location.reload(true)
   }
 
   return (
     <div className="header-wrapper">
       <Container>
-        <div className={`modal ${isOpen ? 'visible' : ""}`}>
+        <div className={`modal ${isOpen ? "visible" : ""}`}>
           <div className="modal-wrapper">
             <h3 className="modal-heading">Вход на udevs news</h3>
             <form className="modal-form" onSubmit={onSubmit}>
@@ -47,11 +53,17 @@ const Header = () => {
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <button type="submit" className="modal-button">Register</button>
+              <button type="submit" className="modal-button">
+                Register
+              </button>
             </form>
           </div>
         </div>
-        {isOpen && <button onClick={() => setIsOpen(false)}><div className="overlay"/></button> }
+        {isOpen && (
+          <button onClick={() => setIsOpen(false)}>
+            <div className="overlay" />
+          </button>
+        )}
         <div className="header-menu-wrapper">
           <div className="logo">
             <Link to="/">
@@ -64,9 +76,29 @@ const Header = () => {
               alt="notifications"
               className="notifications"
             />
-            {!isAuthorized && <button className="header-menu-button" onClick={() => setIsOpen(true)}>Войти</button>}
-           {isAuthorized &&  <img src="/images/person.jpg" alt="person-img" className="persons-img-logged-in" />}
-           {isAuthorized &&  <button className="log-out-btn" onClick={() => localStorage.removeItem('isAuthorized')}>Log Out</button>}
+            {!isAuthorized && (
+              <button
+                className="header-menu-button"
+                onClick={() => setIsOpen(true)}
+              >
+                Войти
+              </button>
+            )}
+            {isAuthorized && (
+              <img
+                src="/images/person.jpg"
+                alt="person-img"
+                className="persons-img-logged-in"
+              />
+            )}
+            {isAuthorized && (
+              <button
+                className="log-out-btn"
+                onClick={logOut}
+              >
+                Log Out
+              </button>
+            )}
           </div>
         </div>
       </Container>
